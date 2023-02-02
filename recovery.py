b8cec55345dae96db4e266b5c0ed2be381718697
GIUSEPPE PELLEGRINO s303999 <s303999@studenti.polito.it>
2023-02-02 17:04:57 +0100

    .

--- recovery.py
+++ recovery.py
@@ -6,10 +6,16 @@ from PIL import Image
 from models.cifar100.resnet20 import ClientModel as res20
 from train_resnet20 import evaluate
 import inversefed
+import os
+import torchvision
+import datetime
+import time
 
+start_time = time.time()
 num_images = 1
 trained_model = True
-
+target_id = -1
+image_path = 'images/'
 
 setup = inversefed.utils.system_startup()
 defs = inversefed.training_strategy('conservative')
@@ -43,10 +49,27 @@ def plot(tensor):
 
 
 if num_images == 1:
-    ground_truth_image = torch.as_tensor(np.array(Image.open("auto.jpg").resize((32, 32), Image.BICUBIC)) / 255, 
-                                         **setup)
-    ground_truth = ground_truth_image.permute(2, 0, 1).sub(dm).div(ds).unsqueeze(0).contiguous()
-    labels = torch.as_tensor((1,), device=setup['device'])
+    if target_id == -1:  # demo image
+        # Specify PIL filter for lower pillow versions
+        ground_truth = torch.as_tensor(
+            np.array(Image.open("auto.jpg").resize((32, 32), Image.BICUBIC)) / 255, **setup
+        )
+        ground_truth = ground_truth.permute(2, 0, 1).sub(dm).div(ds).unsqueeze(0).contiguous()
+        
+        labels = torch.as_tensor((1,), device=setup["device"])
+        target_id = -1
+    else:
+        #Se il target è none prende un immagine a caso altrimenti quella dell'id scelto
+        if target_id is None:
+            target_id = np.random.randint(len(validloader.dataset))
+        else:
+            target_id = target_id
+        ground_truth, labels = validloader.dataset[target_id]
+
+        ground_truth, labels = (
+            ground_truth.unsqueeze(0).to(**setup),
+            torch.as_tensor((labels,), device=setup["device"]),
+        )
 else:
     ground_truth, labels = [], []
     idx = 25 # choosen randomly ... just whatever you want
@@ -60,8 +83,6 @@ else:
     labels = torch.cat(labels)
 
 
-plot(ground_truth);
-print([validloader.dataset.classes[l] for l in labels]);
 
 model.zero_grad()
 target_loss, _, _ = loss_fn(model(ground_truth), labels)
@@ -90,6 +111,24 @@ test_mse = (output.detach() - ground_truth).pow(2).mean()
 feat_mse = (model(output.detach())- model(ground_truth)).pow(2).mean()  
 test_psnr = inversefed.metrics.psnr(output, ground_truth, factor=1/ds)
 
-plot(output)
-plt.title(f"Rec. loss: {stats['opt']:2.4f} | MSE: {test_mse:2.4f} "
-          f"| PSNR: {test_psnr:4.2f} | FMSE: {feat_mse:2.4e} |");
+print('output')
+
+os.makedirs(image_path, exist_ok=True)
+output_denormalized = torch.clamp(output * ds + dm, 0, 1)
+rec_filename = (
+    f'{validloader.dataset.classes[labels][0]}'
+    f"res20_{target_id}.png"
+)
+torchvision.utils.save_image(output_denormalized, os.path.join(image_path, rec_filename))
+
+gt_denormalized = torch.clamp(ground_truth * ds + dm, 0, 1)
+gt_filename = f"{validloader.dataset.classes[labels][0]}_ground_truth-{target_id}.png"
+torchvision.utils.save_image(gt_denormalized, os.path.join(image_path, gt_filename))
+
+print(f"Rec. loss: {stats['opt']:2.4f} | MSE: {test_mse:2.4f} | PSNR: {test_psnr:4.2f} | FMSE: {feat_mse:2.4e} |")
+
+ # Print final timestamp
+print(datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p"))
+print("---------------------------------------------------")
+print(f"Finished computations with time: {str(datetime.timedelta(seconds=time.time() - start_time))}")
+print("-------------Job finished.-------------------------")
